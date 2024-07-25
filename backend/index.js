72d2788a8bd6c00f90ac0b69ead57e7f73eaf630
vishal086 <vishalkumar359877@gmail.com>
2024-07-25 19:18:58 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'*',
+    origin:'https://chatapp-frontend-c1xz.onrender.com/',
     credentials:true
 };
 app.use(cors(corsOption)); 
